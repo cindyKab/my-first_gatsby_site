dd57e28b1c97b31534cf6d6f8006f655d5779f4f
cindy <cindy.kabeya@efrei.net>
2021-09-15 22:41:35 +0200

    Finished Gatsby tutorial part 2

---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layouts'
+
+
+const AboutPage =() =>{
+    return(
+/*         <main>
+            <title> About Me</title>
+            <h1> C'est moi Cindy K</h1>
+            <p> 1.75 86kg haha </p>
+            <Link to ="/"> Back Home</Link>
+        </main> */
+        <Layout pageTitle="About me">
+            <p> Hi there, I'm cindy Kabe Bilo </p>
+        </Layout>
+    )
+}
+
+export default AboutPage
